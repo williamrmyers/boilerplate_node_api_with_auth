b3a5d18cdcf48dd366931d2da80d7e368c865e7e
William Myers <williamrmyers@gmail.com>
2018-06-22 16:36:07 -0700

    CORS Config

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ let {authenticate} = require('./middleware/authenticate');
 let app = express();
 const port = process.env.PORT;
 
-// For Enabling CORS
-app.all('*', function(req, res, next) {
-     var origin = req.get('origin');
-     res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     next();
+
+app.use(function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+       res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+          next();
 });
+
 
 app.use(bodyParser.json());
 
