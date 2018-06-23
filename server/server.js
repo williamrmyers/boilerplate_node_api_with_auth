a6390b1ae2bd652d141af60ff6e722065c0a783f
William Myers <williamrmyers@gmail.com>
2018-06-22 17:18:52 -0700

    Modified CORS

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
 app.use(function(req, res, next) {
        res.header("Access-Control-Allow-Origin", "*");
        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-       res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+       res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, GET, DELETE, OPTIONS');
           next();
 });
 
